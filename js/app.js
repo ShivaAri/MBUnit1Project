0b11aedebe64cdcdbbf59566d68f5b08dc243c61
Michael Brown <michael.brown0398@gmail.com>
2024-03-30 10:11:58 -0500

    add reset button and begin javascript coding

--- js/app.js
+++ js/app.js
@@ -5,15 +5,22 @@
 
 
 /*--------------- Variables (state) ----------------*/
-
+let remainingTime, 
 
 
 /*------------- Cached Element References -----------*/
 
-
+const correctBtn = document.getElementById('correct-answer-button')
+const incorrectBtn = document.getElementById('incorrect-answer-button')
+const resetBtn = document.getElementById('reset-button')
 
 /*----------------- Event Listeners ----------------*/
 
 
 
 /*------------------- Functions ---------------------*/
+
+function init() {
+
+}
+
