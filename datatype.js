66bea59138b88c06223a88621f5ecc393be65488
Michael Brown <michael.brown0398@gmail.com>
2024-03-31 13:17:47 -0500

    add handle click function and add data type javascript array

---
+++ datatype.js
@@ -0,0 +1,34 @@
+const correctAnswers = [1985, Sephiroth, Shiva, Street Fighter, Counter the Pokémon not the Tera Jewel, Inuyasha, Dance With The IdleDeadline, Perfect Dark, Erudite Eyes]
+
+const incorrectAnswers = [1998, 1987, Kazuya Mishima, Ridley, Vishnu, Brahma, Fatal Fury, Tekken, Counter the Tera Jewel, Stay Idle, Dragon Ball Z, Ghost Sweeper Mikami, VHS Glitch, GoldenEye 007, Smash Bros, Unseen Entities, Beat Eat Nest]
+
+const questions = [
+  {
+    question: 'In what year was Shin Megami Tensei born?', correctAnswer: '1985', incorrectAnswers: ['1998', '1987']
+    
+  },
+  {
+    question: 'Which character said this quote: "Watashi wa...omoide ni wa naranai sa..." [I will never...be a memory]', correctAnswer: 'Sephiroth', incorrectAnswers: ['Kazuya Mishima', 'Ridley']
+  },
+  {
+    question: 'Of the Hindu Trimurti, which of the three is known as the Destroyer AND the Lord of Dance?', correctAnswer: 'Shiva', incorrectAnswers: ['Vishnu', 'Brahma']
+  },
+  {
+    question: 'What is the name of the game series Ryu comes from?', correctAnswer: 'Street Fighter', incorrectAnswers: ['Fatal Fury'. 'Tekken']
+  },
+  {
+    question: 'When dealing with tera raids in Pokémon Scarlet and Violet, what is the best way to win them?', correctAnswer: 'Counter the Pokémon not the Tera Jewel', incorrectAnswers: ['Counter the Tera Jewel'. 'Stay Idle']
+  },
+  {
+    question: 'Which classic anime showcases an Inu Hanyo and his group on a mission to restore the Shikon Jewel?', correctAnswer: 'Inuyasha', incorrectAnswers: ['Dragon Ball Z'. 'Ghost Sweeper Mikami']
+  },
+  {
+    question: 'What Electronic band is responsible for songs such as Sledge, Not Of This Earth, and That House?', correctAnswer: 'Dance With The Dead', incorrectAnswers: ['VHS Glitch'. 'Mega Drive']
+  },
+  {
+    question: 'The song A Corpse In The Trunk by Nightcrawler is a remix of an old song from what game?', correctAnswer: 'Perfect Dark', incorrectAnswers: ['GoldenEye 007', 'Smash Bros']
+  },
+  {
+    question: 'In various fighting games, characters have their own theme songs. What is the name of the theme song of Chaos from Under Night Inbirth?', correctAnswer: 'Erudite Eyes', incorrectAnswers: ['Unseen Entities'. 'Beat Eat Nest']
+  },
+]
--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ let correctAnswerCount = 0
 let incorrectAnswerCount = 0
 
 /*------------- Cached Element References -----------*/
+const answerEl = document.getElementById('answer-count')
 const messageEl = document.getElementById('message')
 const correctBtn = document.getElementById('correct-answer-button')
 const incorrectBtn = document.getElementById('incorrect-answer-button')
@@ -27,7 +28,9 @@ init()
 
 
 function init() {
-
+  render()
+  correctAnswer = 0
+  incorrectAnswer = 0
 }
 
 function tickTock() {
@@ -48,10 +51,24 @@ function showMessage(message) {
   messageEl.textContent = message
 }
 
-function handleClick() {
+function handleClick(evt) {
   
+  const correctButtonIdx = evt.target.id('correct-answer-button')
+  const wrongButtonIdx = evt.target.id ('incorrect-answer-button')
+  if (evt.target.id === 'correct-answer-button') {
+    correctAnswer = correctAnswer + 1
+  } else if(evt.target.id === 'incorrect-answer-button') {
+    incorrectAnswer = incorrectAnswer + 1
+  }
+  render()
 }
 
 function render() {
+  updateMessage()
 
+}
+
+function updateMessage() {
+  messageEl.textContent = `${correctAnswer} ${incorrectAnswer}`
+  
 }
