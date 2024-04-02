85df4201f3e6e3b36a4c450ce089f045c89d2f4e
Michael Brown <michael.brown0398@gmail.com>
2024-04-02 17:34:05 -0500

    update javascript with datatype file imported and function updates

---
+++ data/datatype.js
@@ -0,0 +1,39 @@
+
+const questions = [
+  {
+    question: 'In what year was Shin Megami Tensei born?', correctAnswer: '1985', incorrectAnswer1: '1967', incorrectAnswers2: '1995'
+    
+  },
+  {
+    question: 'Which character said this quote: "Watashi wa...omoide ni wa naranai sa..." [I will never...be a memory]', correctAnswer: 'Sephiroth', incorrectAnswer1: 'Kazuya Mishima', incorrectAnswer2: 'Ridley'
+  },
+  {
+    question: 'Of the Hindu Trimurti, which of the three is known as the Destroyer AND the Lord of Dance?', correctAnswer: 'Shiva', incorrectAnswer1: 'Vishnu', incorrectAnswer2: 'Brahman'
+  },
+  {
+    question: 'What is the name of the game series Ryu comes from?', correctAnswer: 'Street Fighter', incorrectAnswer1: 'Fatal Fury', incorrectAnswer2: 'Tekken'
+  },
+  {
+    question: 'When dealing with tera raids in Pokémon Scarlet and Violet, what is the best way to win them?', correctAnswer: 'Counter the Pokémon not the Tera Jewel', incorrectAnswer1: 'Counter The Tera Jewel', incorrectAnswer2: 'Stay Idle'
+  },
+  {
+    question: 'Which classic anime showcases an Inu Hanyo and his group on a mission to restore the Shikon Jewel?', correctAnswer: 'Inuyasha', incorrectAnswer1: 'Dragon Ball Z', incorrectAnswer2: 'Ghost Sweeper Mikami'
+  },
+  {
+    question: 'What Electronic band is responsible for songs such as Sledge, Not Of This Earth, and That House?', correctAnswer: 'Dance With The Dead', incorrectAnswer1: 'VHS Glitch', incorrectAnswer2: 'Mega Drive'
+  },
+  {
+    question: 'The song A Corpse In The Trunk by Nightcrawler is a remix of an old song from what game?', correctAnswer: 'Perfect Dark', incorrectAnswer1: 'GoldenEye 007', incorrectAnswers2: 'Smash Bros'
+  },
+  {
+    question: 'In various fighting games, characters have their own theme songs. What is the name of the theme song of Chaos from Under Night Inbirth?', correctAnswer: 'Erudite Eyes', incorrectAnswer1: 'Unseen Entities', incorrectAnswer2: 'Beat Eat Nest'
+  },
+]
+
+function getRandomQuestion() {
+  return questions[Math.floor(Math.random() * questions.length)]
+}
+
+export {
+  getRandomQuestion,
+}
--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /*------------------ Constants --------------------*/
-import { getRandomQuestion } from "../datatype.js"
+import { getRandomQuestion } from "../data/datatype.js"
 const whoWantSomeDuckSound = new Audio('../audio/Who want some duck.mp3')
 const shockSound = new Audio('../audio/shock.mp3')
 const ohSound = new Audio('../audio/OH.mp3')
@@ -12,7 +12,7 @@ let gameInPlay, timerIntervalId, correctAnswer, incorrectAnswer, noClickyYet
 let correctAnswerCount = 0
 let incorrectAnswerCount = 0
 let timeLeft = 120
-
+const questions = []
 let timer = setInterval(function() {
   countdownEl.textContent = timeLeft + ' seconds remain...'
   timeLeft -= 1
@@ -31,10 +31,12 @@ const correctBtn = document.getElementById('correct-answer-button')
 const incorrectBtn = document.getElementById('incorrect-answer-button')
 const resetBtn = document.getElementById('reset-button')
 const countdownEl = document.getElementById('countdown')
+const questionContainer = document.querySelector('#question-container')
+const questionBtn = document.querySelector('#question-button')
 /*----------------- Event Listeners ----------------*/
 resetBtn.addEventListener('click', init)
-correctBtn.addEventListener('click', handleClick)
-incorrectBtn.addEventListener('click', handleClick)
+
+questionBtn.addEventListener('click', createQuestion)
 
 /*------------------- Functions ---------------------*/
 init()
@@ -45,6 +47,7 @@ function init() {
   correctAnswer = 0
   incorrectAnswer = 0
   gameInPlay = false
+
 }
 
 
@@ -70,7 +73,35 @@ function handleClick(evt) {
 }
 
 function render() {
-  
+  questionContainer.innerHTML = ''
+  questions.forEach((question, idx) => {
+    appendQuestion(question, idx)
+  })
+}
+
+function appendQuestion(question) {
+  let questionCard = document.createElement('div')
+  questionCard.className = `card ${question.question}`
+  questionCard.innerHTML = 
+  `<div>
+  <p>${question.text}</p>
+  <p id = 'question-container'>img src='../js/datatype.js
+  </div>
+  <footer>
+    <button class = 'correct-answer-button'>${question.correctAnswer}</button>
+    <button class = 'incorrect-answer-button'>${question.incorrectAnswer1}</button>
+    <button class = 'incorrect-answer-button'>${question.incorrectAnswer2}</button>
+    </footer>
+    `
+
+    questionContainer.appendChild(questionCard)
+}
+
+function createQuestion() {
+  const newQuestion = getRandomQuestion()
+
+  questions.push(newQuestion)
+  render()
 }
 
 function updateMessage() {
